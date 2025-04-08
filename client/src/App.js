3a39d12f0f59ce3213c931947a94ecad174ac233
Catherine <cayeronwi@gmail.com>
2025-04-08 14:27:33 +0000

    app commit802

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
 import Navbar from './Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
+import Create from './Create';
+import Update from './Create';
 
 
 function App() {
+  return (
+    <div className='App'>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Member />}></Route>
+          <Route path='/create' element={<Create />}></Route>
+          <Route path='/update/:id' element={<Update />}></Route>
+        </Routes>
+      </BrowserRouter>
 
+    </div>
+  )
 
 }
 
